53082529f70ed133eb7552cad599fdeb6c22ebd5
Kace Cottam <kace.cottam@wsu.edu>
2021-06-09 20:13:25 -0700

    Fixed pings

--- ping_roles.py
+++ ping_roles.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext.commands import context
 from settings import BOT_ROLES_DB
 import api
 
@@ -19,10 +20,11 @@ def setup(bot):
 
         embed = discord.Embed(description="> " + message.content, color=discord.Color.random())
         embed.set_author(name=message.author.display_name, url=message.jump_url, icon_url=message.author.avatar_url)
-
+        mentions = ''
         for role in gameRoles:
             users = api.listUsers(cur, message.guild.id, role.id)
             content = ' '.join( userRoles[user].mention for user in users )
+            mentions += content + ' '
             embed.add_field(name=role.name, value=content)
 
-        await message.channel.send(embed=embed)
+        await message.channel.send(content=mentions, embed=embed)
