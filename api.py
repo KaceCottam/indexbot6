cf25f5a7dcae34a21a570d7f8b9ab47cccf467bf
Kace Cottam <kace.cottam@wsu.edu>
2021-12-04 12:05:23 -0800

    update bot api and add example sql

--- api.py
+++ api.py
@@ -1,47 +1,38 @@
-import sqlite3
-from typing import List, Optional, Union
+import peewee
+from peewee import SQL, Model, SqliteDatabase, TextField
 
-# TODO be safe with no fstrings
-# given that this only works with message ids, it would be difficult to use this database unsafely.
+database = SqliteDatabase(None)
 
-def makeApi(path, timeout: int = 1000):
-    con = sqlite3.connect(path, timeout=timeout)
-    cur = con.cursor()
-    return (con, cur)
+def InitDB(path: str) -> None:
+    """Initialize the database
 
-def ensureTableExists(cursor: sqlite3.Cursor, guildid: int) -> None:
-    cursor.execute(fr"CREATE TABLE IF NOT EXISTS guild_{guildid} (roleid INT, userid INT, UNIQUE(roleid, userid))")
+    Args:
+        path (str): path to the database file
+    """
+    database.init(path)
+    
+class Roles(Model):
+    guildid = TextField(primary_key=True)
+    roleid  = TextField()
+    userid  = TextField()
+    class Meta:
+        database = database
+        constraints = [SQL("UNIQUE (roleid, userid)")]
 
-def addRole(cursor: sqlite3.Cursor, guildid: int, roleid: int, userid: int) -> Optional[str]:
-    try:
-        cursor.execute(fr"INSERT INTO guild_{guildid} VALUES({roleid}, {userid})")
-    except sqlite3.IntegrityError:
-        return "That user is already there!"
+def AddUser(guildid: str, roleid: str, userid: str) -> Roles:
+    """Add a user to a guild
 
-def listUsers(cursor: sqlite3.Cursor, guildid: int, roleid: int) -> List[int]:
-    cursor.execute(fr"SELECT userid FROM guild_{guildid} WHERE roleid={roleid}")
-    return [ i[0] for i in cursor.fetchall() ]
+    Requires database to be initialized.
+    
 
-def removeUserFromRole(cursor: sqlite3.Cursor, guildid: int, roleid: int, userid: int) -> Union[str, int]:
-    try:
-        cursor.execute(fr"SELECT COUNT(*) FROM GUILD_{guildid} WHERE roleid={roleid}")
-        if (int(cursor.fetchone()[0]) == 0):
-            return None, "That role doesn't exist!"
-        cursor.execute(fr"DELETE FROM guild_{guildid} WHERE roleid={roleid} AND userid={userid}")
-        cursor.execute(fr"SELECT COUNT(*) FROM guild_{guildid} WHERE roleid={roleid}")
-        if (int(cursor.fetchone()[0]) == 0):
-            return roleid, None
-        return None, None
-    except sqlite3.ProgrammingError:
-        return None, "That query doesn't exist!"
+    Args:
+        guildid (str): guild id
+        roleid (str):  role id
+        userid (str):  user id
+    """
+    user = Roles.create(guildid=guildid, roleid=roleid, userid=userid)
+    user.save()
+    return user
 
-def listRoles(cursor: sqlite3.Cursor, guildid: int, userid: int) -> List[int]:
-    cursor.execute(fr"SELECT roleid FROM guild_{guildid} WHERE userid={userid}")
-    return [ i[0] for i in cursor.fetchall() ]
-
-def listAllRoles(cursor: sqlite3.Cursor, guildid: int) -> List[int]:
-    cursor.execute(fr"SELECT roleid FROM guild_{guildid}")
-    return [ i[0] for i in cursor.fetchall() ]
-
-def removeRole(cursor: sqlite3.Cursor, guildid: int, roleid: int):
-    cursor.execute(fr"DELETE FROM guild_{guildid} WHERE roleid={roleid}")
+def ListUsers(guildid: str, roleid: str) -> list[Roles] | None:
+    pass
--- settings.py
+++ settings.py
@@ -1,21 +1,19 @@
 import os
 import sys
 from dotenv import load_dotenv
-from pathlib import Path
 
 load_dotenv()
 
 BOT_APPLICATION_ID = os.environ.get("BOT_APPLICATION_ID")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-BOT_ROLES_DB = Path(os.environ.get("BOT_ROLES_DB") or "./roles.db")
-BOT_GUILD_IDS = [ int(i) for i in ids.split() ] if (ids := os.environ.get("BOT_GUILD_IDS")) else None
+BOT_TOKEN          = os.environ.get("BOT_TOKEN")
+BOT_ROLES_DB       = os.environ.get("BOT_ROLES_DB") or "./roles.db"
 
 if BOT_APPLICATION_ID is None:
-    print("APPLICATION_ID not loaded! Aborting...", file=sys.stderr)
-    sys.exit(1)
+    print("APPLICATION_ID not loaded!", file=sys.stderr)
+
 if BOT_TOKEN is None:
-    print("TOKEN not loaded! Aborting...", file=sys.stderr)
-    sys.exit(1)
-if BOT_GUILD_IDS is None:
-    print("BOT_GUILD_IDS not loaded! Aborting...", file=sys.stderr)
+    print("TOKEN not loaded!", file=sys.stderr)
+
+if any(x is None for x in BOT_APPLICATION_ID, BOT_TOKEN):
+    print("Aborting...")
     sys.exit(1)
