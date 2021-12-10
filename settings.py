1fb11300bd4a5f55588ebac4f91de4ecb5dbb50a
Kace Cottam <kace.cottam@wsu.edu>
2021-12-09 20:09:40 -0800

    update requirements

--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_APPLICATION_ID: str = os.environ.get("BOT_APPLICATION_ID")
-BOT_TOKEN: str = os.environ.get("BOT_TOKEN")
-BOT_ROLES_DB: str = os.environ.get("BOT_ROLES_DB") or "./roles.db"
+BOT_APPLICATION_ID = os.environ.get("BOT_APPLICATION_ID")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_ROLES_DB = os.environ.get("BOT_ROLES_DB") or "./roles.db"
 
 if BOT_APPLICATION_ID is None:
     print("APPLICATION_ID not loaded!", file=sys.stderr)
