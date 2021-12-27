e0bca3c2e0e753a907a47db0943028110ca8e4ed
Kace Cottam <kace.cottam@wsu.edu>
2021-12-27 02:02:46 -0800

    back to a workable state; all the commands have starter code, and /game join, onMessageCreate and onRead is implemented

--- src/api.rs
+++ src/api.rs
@@ -1,25 +1,33 @@
 #![macro_use]
+#![warn(rustdoc::all)]
 
-use std::io::BufReader;
+use serde_derive::{Deserialize, Serialize};
 use std::fs::File;
+use std::io::BufReader;
 use std::path::Path;
-use serde_derive::{Serialize, Deserialize};
-use tqdb::{Database, search, remove, Query};
 use thiserror::Error;
+use tqdb::{remove, search, Database, Query};
 
-pub type Snowflake = String;
+pub type Snowflake = u64;
 pub type GuildId = Snowflake;
 pub type RoleId = Snowflake;
 pub type UserId = Snowflake;
 
-#[derive(Debug, Eq, PartialEq, Hash, Serialize, Deserialize, Clone)]
+#[derive(Debug, Eq, PartialEq, Hash, Serialize, Deserialize, Clone, Default)]
 pub struct Roles {
     guild_id: GuildId,
     role_id: RoleId,
     user_id: UserId,
 }
 
+unsafe impl Send for Roles {}
+unsafe impl Sync for Roles {}
+
+#[derive(Clone, Debug, Eq, PartialEq, Default)]
 pub struct RolesDatabase(Database<Roles>);
+
+unsafe impl Send for RolesDatabase {}
+unsafe impl Sync for RolesDatabase {}
 
 #[derive(Error, Debug)]
 pub enum ApiError {
@@ -29,59 +37,88 @@ pub enum ApiError {
     BadRead,
     #[error("Item doesn't exist")]
     Removal,
+    #[error("Couldn't save the database")]
+    BadSave,
 }
 
 type Result<T> = core::result::Result<T, ApiError>;
 
 impl RolesDatabase {
-    pub fn from(filename: &Path) -> Result<Self> {
-        let file = File::open(filename).map_err(|_| ApiError::BadRead)?;
+    pub fn try_from<'a>(filename: impl Into<&'a Path>) -> Result<Self> {
+        let file = File::open(filename.into()).map_err(|_| ApiError::BadRead)?;
         let br = BufReader::new(file);
         let db = Database::try_from(br).map_err(|_| ApiError::BadRead)?;
         Ok(Self { 0: db })
     }
-    pub fn add_user_to_role<G: Into<GuildId>, R: Into<RoleId>, U: Into<UserId>>(&mut self, guild_id: G, role_id: R, user_id: U) -> Result<()> {
+    pub fn add_user_to_role<G: Into<GuildId>, R: Into<RoleId>, U: Into<UserId>>(
+        &mut self,
+        guild_id: G,
+        role_id: R,
+        user_id: U,
+    ) -> Result<()> {
         let guild_id = guild_id.into();
         let role_id = role_id.into();
         let user_id = user_id.into();
-        self.0.insert_unique(Roles { guild_id, role_id, user_id }).map_err(|_| ApiError::Insertion)
+        self.0
+            .insert_unique(Roles {
+                guild_id,
+                role_id,
+                user_id,
+            })
+            .map_err(|_| ApiError::Insertion)
     }
-    pub fn show_roles_of_user<G: Into<GuildId>, U: Into<UserId>>(&self, guild_id: G, user_id: U) -> Vec<&RoleId> {
+    pub fn show_roles_of_user<G: Into<GuildId>, U: Into<UserId>>(
+        &self,
+        guild_id: G,
+        user_id: U,
+    ) -> Vec<&RoleId> {
         let guild_id = guild_id.into();
         let user_id = user_id.into();
         search!(&self.0 => move |it: &Roles| it.guild_id == guild_id && it.user_id == user_id)
             .map(|it| &it.role_id)
             .collect()
     }
-    pub fn show_users_of_role<G: Into<GuildId>, R: Into<RoleId>>(&self, guild_id: G, role_id: R) -> Vec<&UserId> {
+    pub fn show_users_of_role<G: Into<GuildId>, R: Into<RoleId>>(
+        &self,
+        guild_id: G,
+        role_id: R,
+    ) -> Vec<&UserId> {
         let guild_id = guild_id.into();
         let role_id = role_id.into();
         let mut res =
             search!(&self.0 => move |it: &Roles| it.guild_id == guild_id && it.role_id == role_id)
-            .map(|it| &it.user_id)
-            .collect::<Vec<&UserId>>();
+                .map(|it| &it.user_id)
+                .collect::<Vec<&UserId>>();
         res.dedup();
         res
     }
     pub fn show_roles_of_guild<G: Into<GuildId>>(&self, guild_id: G) -> Vec<&RoleId> {
         let guild_id = guild_id.into();
-        let mut res =
-            search!(&self.0 => move |it: &Roles| it.guild_id == guild_id)
+        let mut res = search!(&self.0 => move |it: &Roles| it.guild_id == guild_id)
             .map(|it| &it.role_id)
             .collect::<Vec<&UserId>>();
         res.dedup();
         res
     }
-    pub fn remove_user_from_role<G: Into<GuildId>, R: Into<RoleId>, U: Into<UserId>>(&mut self, guild_id: G, role_id: R, user_id: U) -> Result<UserId> {
+    pub fn remove_user_from_role<G: Into<GuildId>, R: Into<RoleId>, U: Into<UserId>>(
+        &mut self,
+        guild_id: G,
+        role_id: R,
+        user_id: U,
+    ) -> Result<UserId> {
         let guild_id = guild_id.into();
         let role_id = role_id.into();
         let user_id = user_id.into();
         remove!(&mut self.0 => move |it: &Roles| it.guild_id == guild_id && it.role_id == role_id && it.user_id == user_id )
             .next()
             .map(|it| it.user_id)
-            .ok_or_else(|| ApiError::Removal)
+            .ok_or(ApiError::Removal)
     }
-    pub fn remove_role<G: Into<GuildId>, R: Into<RoleId>>(&mut self, guild_id: G, role_id: R) -> Result<Vec<UserId>> {
+    pub fn remove_role<G: Into<GuildId>, R: Into<RoleId>>(
+        &mut self,
+        guild_id: G,
+        role_id: R,
+    ) -> Result<Vec<UserId>> {
         let guild_id = guild_id.into();
         let role_id = role_id.into();
         let users =
@@ -94,7 +131,11 @@ impl RolesDatabase {
             Ok(users)
         }
     }
-    pub fn remove_user<G: Into<GuildId>, U: Into<UserId>>(&mut self, guild_id: G, user_id: U) -> Result<Vec<RoleId>> {
+    pub fn remove_user<G: Into<GuildId>, U: Into<UserId>>(
+        &mut self,
+        guild_id: G,
+        user_id: U,
+    ) -> Result<Vec<RoleId>> {
         let guild_id = guild_id.into();
         let user_id = user_id.into();
         let roles =
@@ -109,118 +150,157 @@ impl RolesDatabase {
     }
     pub fn remove_guild<G: Into<GuildId>>(&mut self, guild_id: G) -> Result<Vec<Roles>> {
         let guild_id = guild_id.into();
-        let roles =
-            remove!(&mut self.0 => move |it: &Roles| it.guild_id == guild_id )
-                .collect::<Vec<Roles>>();
+        let roles = remove!(&mut self.0 => move |it: &Roles| it.guild_id == guild_id )
+            .collect::<Vec<Roles>>();
         if roles.is_empty() {
             Err(ApiError::Removal)
         } else {
             Ok(roles)
         }
     }
+    pub fn save<P: AsRef<Path>>(&self, filename: P) -> Result<()> {
+        self.0.save_to_file(filename).or(Err(ApiError::BadSave))
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use tqdb::Database;
     use crate::api::*;
+    use tqdb::Database;
 
     fn create_test_db() -> RolesDatabase {
         RolesDatabase {
-            0:
-            Database::from(vec![
-                Roles { guild_id: "1".into(), role_id: "1".into(), user_id: "1".into() },
-                Roles { guild_id: "1".into(), role_id: "1".into(), user_id: "2".into() },
-                Roles { guild_id: "1".into(), role_id: "1".into(), user_id: "3".into() },
-                Roles { guild_id: "1".into(), role_id: "1".into(), user_id: "4".into() },
-                Roles { guild_id: "1".into(), role_id: "1".into(), user_id: "5".into() },
-                Roles { guild_id: "1".into(), role_id: "2".into(), user_id: "1".into() },
-                Roles { guild_id: "1".into(), role_id: "2".into(), user_id: "2".into() },
-                Roles { guild_id: "1".into(), role_id: "2".into(), user_id: "6".into() },
-                Roles { guild_id: "2".into(), role_id: "1".into(), user_id: "1".into() },
-                Roles { guild_id: "2".into(), role_id: "1".into(), user_id: "6".into() },
-                Roles { guild_id: "2".into(), role_id: "3".into(), user_id: "7".into() },
-            ])
+            0: Database::from(vec![
+                Roles {
+                    guild_id: 1u64,
+                    role_id: 1u64,
+                    user_id: 1u64,
+                },
+                Roles {
+                    guild_id: 1u64,
+                    role_id: 1u64,
+                    user_id: 2u64,
+                },
+                Roles {
+                    guild_id: 1u64,
+                    role_id: 1u64,
+                    user_id: 3u64,
+                },
+                Roles {
+                    guild_id: 1u64,
+                    role_id: 1u64,
+                    user_id: 4u64,
+                },
+                Roles {
+                    guild_id: 1u64,
+                    role_id: 1u64,
+                    user_id: 5u64,
+                },
+                Roles {
+                    guild_id: 1u64,
+                    role_id: 2u64,
+                    user_id: 1u64,
+                },
+                Roles {
+                    guild_id: 1u64,
+                    role_id: 2u64,
+                    user_id: 2u64,
+                },
+                Roles {
+                    guild_id: 1u64,
+                    role_id: 2u64,
+                    user_id: 6u64,
+                },
+                Roles {
+                    guild_id: 2u64,
+                    role_id: 1u64,
+                    user_id: 1u64,
+                },
+                Roles {
+                    guild_id: 2u64,
+                    role_id: 1u64,
+                    user_id: 6u64,
+                },
+                Roles {
+                    guild_id: 2u64,
+                    role_id: 3u64,
+                    user_id: 7u64,
+                },
+            ]),
         }
     }
 
     #[test]
     pub fn test_add_user_to_role() {
         let mut db = create_test_db();
-        assert!(db.add_user_to_role("1", "1", "1").is_err());
-        assert!(db.add_user_to_role("1", "1", "1000").is_ok());
+        assert!(db.add_user_to_role(1u64, 1u64, 1u64).is_err());
+        assert!(db.add_user_to_role(1u64, 1u64, 1000u64).is_ok());
     }
 
     #[test]
     pub fn test_show_roles_of_user() {
         let db = create_test_db();
-        assert_eq!(
-            db.show_roles_of_user("1", "2"),
-            vec![&"1".to_string(), &"2".to_string()]
-        )
+        assert_eq!(db.show_roles_of_user(1u64, 2u64), vec![&1u64, &2u64])
     }
 
     #[test]
     pub fn test_show_users_of_role() {
         let db = create_test_db();
-        assert_eq!(
-            db.show_users_of_role("1", "2"),
-            vec![&"1".to_string(), &"2".to_string(), &"6".to_string()]
-        )
+        assert_eq!(db.show_users_of_role(1u64, 2u64), vec![&1u64, &2u64, &6u64])
     }
 
     #[test]
     pub fn test_show_roles_of_guild() {
         let db = create_test_db();
-        assert_eq!(
-            db.show_roles_of_guild("1"),
-            vec![&"1".to_string(), &"2".to_string()]
-        );
-        assert_eq!(
-            db.show_roles_of_guild("2"),
-            vec![&"1".to_string(), &"3".to_string()]
-        );
+        assert_eq!(db.show_roles_of_guild(1u64), vec![&1u64, &2u64]);
+        assert_eq!(db.show_roles_of_guild(2u64), vec![&1u64, &3u64]);
     }
 
     #[test]
     pub fn test_remove_user_from_role() {
         let mut db = create_test_db();
-        assert!(db.remove_user_from_role("1", "1", "2").is_ok());
-        assert!(db.remove_user_from_role("1", "10000", "2").is_err());
+        assert!(db.remove_user_from_role(1u64, 1u64, 2u64).is_ok());
+        assert!(db.remove_user_from_role(1u64, 10000u64, 2u64).is_err());
     }
 
     #[test]
     pub fn test_remove_role() {
         let mut db = create_test_db();
 
-        assert_eq!(
-            db.remove_role("1", "2").unwrap(),
-            vec!["1".to_string(), "2".to_string(), "6".to_string()]
-        );
-        assert!(db.remove_role("1", "10000").is_err());
+        assert_eq!(db.remove_role(1u64, 2u64).unwrap(), vec![1u64, 2u64, 6u64]);
+        assert!(db.remove_role(1u64, 10000u64).is_err());
     }
 
     #[test]
     pub fn test_remove_user() {
         let mut db = create_test_db();
-        assert_eq!(
-            db.remove_user("1", "2").unwrap(),
-            vec!["1".to_string(), "2".to_string()]
-        );
-        assert!(db.remove_user("1", "200000").is_err());
+        assert_eq!(db.remove_user(1u64, 2u64).unwrap(), vec![1u64, 2u64]);
+        assert!(db.remove_user(1u64, 200000u64).is_err());
     }
 
     #[test]
     pub fn test_remove_guild() {
         let mut db = create_test_db();
         assert_eq!(
-            db.remove_guild("2").unwrap(),
+            db.remove_guild(2u64).unwrap(),
             vec![
-                Roles { guild_id: "2".into(), role_id: "1".into(), user_id: "1".into() },
-                Roles { guild_id: "2".into(), role_id: "1".into(), user_id: "6".into() },
-                Roles { guild_id: "2".into(), role_id: "3".into(), user_id: "7".into() },
+                Roles {
+                    guild_id: 2u64,
+                    role_id: 1u64,
+                    user_id: 1u64
+                },
+                Roles {
+                    guild_id: 2u64,
+                    role_id: 1u64,
+                    user_id: 6u64
+                },
+                Roles {
+                    guild_id: 2u64,
+                    role_id: 3u64,
+                    user_id: 7u64
+                },
             ]
         );
-        assert!(db.remove_guild("10000").is_err());
+        assert!(db.remove_guild(10000u64).is_err());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,357 @@
+#![warn(rustdoc::all)]
+#![feature(async_closure)]
+
+use std::path::PathBuf;
+
+use chrono::Utc;
+use dotenv as env;
+use poise::serenity_prelude::{Activity, Mentionable, UserId};
+use poise::{serenity_prelude as serenity, FrameworkOptions, PrefixFrameworkOptions};
+
+use crate::api::RolesDatabase;
+
 mod api;
 
-fn main() {
-    println!("Hello, world!");
+type Data = std::sync::Mutex<RolesDatabase>;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Join the notification list for a role
+#[poise::command(slash_command, category = "game")]
+pub async fn join(
+    ctx: Context<'_>,
+    #[description = "Selected role"] role: serenity::Role,
+) -> Result<(), Error> {
+    let choice = ctx.data().lock().unwrap().add_user_to_role(
+        ctx.guild_id().unwrap(),
+        role.id,
+        ctx.author().id,
+    );
+    match choice {
+        Ok(_) => {
+            ctx.say(format!(
+                "Added {} to role {}!",
+                ctx.author().name,
+                role.name
+            ))
+            .await?;
+            println!(
+                "({}) {} joined {}!",
+                role.guild_id,
+                ctx.author().id,
+                role.id
+            );
+            match ctx
+                .data()
+                .lock()
+                .unwrap()
+                .save(env::var("BOT_ROLES_DB").unwrap())
+            {
+                Err(e) => println!("Error! {}", e),
+                Ok(_) => println!("Saved to {}.", env::var("BOT_ROLES_DB").unwrap()),
+            }
+        }
+        Err(_) => {
+            ctx.say("Error joining role!").await?;
+        }
+    }
+    Ok(())
+}
+
+/// Category
+#[poise::command(slash_command, category = "game")]
+pub async fn game(
+    ctx: Context<'_>,
+    #[description = "Selected game"] game: String,
+) -> Result<(), Error> {
+    ctx.say(format!(
+        "{} wants to create/join game {}!",
+        ctx.author().name,
+        game
+    ))
+    .await?;
+    Ok(())
+}
+
+/// Join or create the notification list for a game
+#[poise::command(slash_command, category = "game")]
+pub async fn create(
+    ctx: Context<'_>,
+    #[description = "Selected game"] game: String,
+) -> Result<(), Error> {
+    ctx.say(format!(
+        "{} wants to create/join game {}!",
+        ctx.author().name,
+        game
+    ))
+    .await?;
+    Ok(())
+}
+
+/// Display the members of a role
+#[poise::command(slash_command, category = "game")]
+pub async fn members(
+    ctx: Context<'_>,
+    #[description = "Selected role"] role: serenity::Role,
+) -> Result<(), Error> {
+    ctx.say(format!(
+        "{} wants to see the role {}!",
+        ctx.author().name,
+        role.mention()
+    ))
+    .await?;
+    Ok(())
+}
+
+/// Leave the notification list for a role
+#[poise::command(slash_command, category = "game")]
+pub async fn leave(
+    ctx: Context<'_>,
+    #[description = "Selected role"] role: serenity::Role,
+) -> Result<(), Error> {
+    ctx.say(format!(
+        "{} wants to leave the role {}!",
+        ctx.author().name,
+        role.mention()
+    ))
+    .await?;
+    Ok(())
+}
+
+/// List the roles that a user will be notified for, or a guild if there is no user.
+#[poise::command(slash_command, category = "game")]
+pub async fn list(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    if let Some(user) = user {
+        ctx.say(format!(
+            "{} wants list the roles of user {}!",
+            ctx.author().name,
+            user.mention()
+        ))
+        .await?;
+    } else {
+        ctx.say(format!(
+            "{} wants list the roles of the server!",
+            ctx.author().name
+        ))
+        .await?;
+    }
+    Ok(())
+}
+
+/// Fetch game deals from isthereanydeal.com for a game
+#[poise::command(slash_command)]
+pub async fn deals(
+    ctx: Context<'_>,
+    #[description = "Selected Game"] game: String,
+) -> Result<(), Error> {
+    ctx.say(format!(
+        "{} wants to see deals for the game \"{}\"!",
+        ctx.author().name,
+        game
+    ))
+    .await?;
+    Ok(())
+}
+
+/// Show this menu
+#[poise::command(prefix_command, track_edits, slash_command)]
+pub async fn help(
+    ctx: Context<'_>,
+    #[description = "Specific command to show help about"] command: Option<String>,
+) -> Result<(), Error> {
+    let config = poise::builtins::HelpConfiguration {
+        extra_text_at_bottom: " Type $help command for more info on a command. You can edit your message to the bot and the bot will edit its response.",
+        ..Default::default()
+    };
+    poise::builtins::help(ctx, command.as_deref(), config).await?;
+    Ok(())
+}
+
+/// Register slash commands in this guild or globally
+///
+/// Run with no arguments to register in guild, run with argument "global" to register globally.
+#[poise::command(prefix_command, hide_in_help)]
+async fn register(ctx: Context<'_>, #[flag] global: bool) -> Result<(), Error> {
+    poise::builtins::register_application_commands(ctx, global).await?;
+    Ok(())
+}
+
+async fn on_error(error: Error, ctx: poise::ErrorContext<'_, Data, Error>) {
+    match ctx {
+        poise::ErrorContext::Setup => panic!("Failed to start bot: {:?}", error),
+        poise::ErrorContext::Command(ctx) => {
+            println!("Error in command `{}`: {:?}", ctx.command().name(), error)
+        }
+        _ => println!("Other error: {:?}", error),
+    }
+}
+
+async fn event_listener(
+    ctx: &serenity::Context,
+    event: &poise::Event<'_>,
+    _framework: &poise::Framework<Data, Error>,
+    user_data: &Data,
+) -> Result<(), Error> {
+    match event {
+        poise::Event::GuildMemberRemoval {
+            guild_id,
+            user,
+            member_data_if_available: _,
+        } => {
+            println!(
+                "Guild ({}) member left: {}#{} ({})",
+                guild_id, user.name, user.discriminator, user.id
+            )
+        }
+        poise::Event::GuildRoleDelete {
+            guild_id,
+            removed_role_id,
+            removed_role_data_if_available,
+        } => {
+            if let Some(role) = removed_role_data_if_available {
+                println!(
+                    "Guild ({}) role deleted {} ({})",
+                    guild_id, role.name, removed_role_id
+                )
+            } else {
+                println!("Guild ({}) role deleted ({})", guild_id, removed_role_id)
+            }
+        }
+        poise::Event::Ready { data_about_bot } => {
+            println!(
+                "Bot {}#{} ({}) connected!",
+                data_about_bot.user.name, data_about_bot.user.discriminator, data_about_bot.user.id
+            );
+            println!("Application id: {}", data_about_bot.application.id);
+            println!("----------");
+            ctx.set_activity(Activity::listening("/help")).await;
+        }
+        poise::Event::Message { new_message } => {
+            let guild_id = match new_message.guild_id {
+                Some(id) => id,
+                None => return Ok(()),
+            };
+
+            let roles = new_message
+                .mention_roles
+                .iter()
+                .map(|id| id.to_role_cached(&ctx).unwrap().name);
+
+            if roles.len() == 0 {
+                return Ok(());
+            }
+
+            let userids = new_message
+                .mention_roles
+                .iter()
+                .flat_map(|id| {
+                    user_data
+                        .lock()
+                        .unwrap()
+                        .show_users_of_role(guild_id.0, id.0)
+                        .into_iter()
+                        .copied()
+                        .collect::<Vec<_>>()
+                })
+                .map(UserId::from)
+                .collect::<Vec<_>>();
+
+            if userids.is_empty() {
+                return Ok(());
+            }
+
+            let thread = match new_message
+                .channel_id
+                .create_public_thread(&ctx, new_message.id, |f| {
+                    f.name(format!(
+                        "[{}] {} Discussion",
+                        Utc::now().format("%v"),
+                        roles.collect::<Vec<String>>().join(", ")
+                    ))
+                    .auto_archive_duration(1440)
+                    .kind(serenity::model::channel::ChannelType::PublicThread)
+                })
+                .await
+            {
+                Ok(t) => t,
+                Err(e) => {
+                    println!(
+                        "Failed to make thread for mention in message ({})! {}",
+                        new_message.id, e
+                    );
+                    return Ok(());
+                }
+            };
+
+            for id in userids.iter() {
+                thread.id.add_thread_member(&ctx, *id).await?;
+            }
+
+            thread
+                .send_message(&ctx, |m| m.content("@everyone"))
+                .await?;
+
+            println!(
+                "Notified roles ({}) in guild ({})!",
+                new_message
+                    .mention_roles
+                    .iter()
+                    .map(|it| it.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" "),
+                guild_id
+            );
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
+
+async fn command_check(ctx: Context<'_>) -> Result<bool, Error> {
+    Ok(ctx.guild_id().is_some())
+}
+
+#[tokio::main]
+async fn main() {
+    env::dotenv().expect("Error loading environment file.");
+    let db_file: PathBuf = env::var("BOT_ROLES_DB")
+        .expect("Expected BOT_ROLES_DB to be set in environment.")
+        .into();
+    let db = RolesDatabase::try_from(db_file.as_path()).unwrap_or_default();
+    let options = FrameworkOptions {
+        prefix_options: PrefixFrameworkOptions {
+            prefix: Some("$".into()),
+            mention_as_prefix: false,
+            ..Default::default()
+        },
+        on_error: |error, ctx| Box::pin(on_error(error, ctx)),
+        listener: |ctx, event, framework, user_data| {
+            Box::pin(event_listener(ctx, event, framework, user_data))
+        },
+        command_check: Some(|ctx| Box::pin(command_check(ctx))),
+        ..Default::default()
+    };
+    poise::Framework::build()
+        .token(env::var("BOT_TOKEN").expect("Expected BOT_TOKEN to be set in environment."))
+        .user_data_setup(move |_ctx, _ready, _framework| {
+            Box::pin(async move { Ok(std::sync::Mutex::from(db)) })
+        })
+        .options(options)
+        .command(game(), |f| {
+            f.subcommand(join(), |f| f)
+                .subcommand(create(), |f| f)
+                .subcommand(members(), |f| f)
+                .subcommand(list(), |f| f)
+                .subcommand(leave(), |f| f)
+        })
+        .command(register(), |f| f)
+        .command(deals(), |f| f)
+        .command(help(), |f| f)
+        .run()
+        .await
+        .expect("Client Error");
 }
